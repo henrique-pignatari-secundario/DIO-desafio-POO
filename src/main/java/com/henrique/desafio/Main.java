a3c0f964d4d264ee536a1fe73416520a89e30828
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-05-21 13:41:52 -0300

    initial commit

---
+++ src/main/java/com/henrique/desafio/Main.java
@@ -0,0 +1,29 @@
+package com.henrique.desafio;
+
+import com.henrique.desafio.dominio.Curso;
+import com.henrique.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("escricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("escricao curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria Java");
+        mentoria.setDescricao("descricao mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
